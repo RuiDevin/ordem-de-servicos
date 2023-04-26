f6eba19b8e5dd58445cd046981c2f1b95138dcae
RuiDevin <rafaelalvaro00@gmail.com>
2023-04-26 11:57:10 -0300

    tela de cadastro 50% 1/5

--- App.js
+++ App.js
@@ -1,20 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button } from 'react-native';
 
-export default function App() {
+const Cadastro = () => {
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+  const [Cpf, setCpf] = useState('');
+
+  const handleCadastro = () => {
+    // validar os campos aqui
+    // enviar as informações para o servidor
+    // mostrar uma mensagem de sucesso ao usuário
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Text>Nome:</Text>
+      <TextInput
+        value={nome}
+        onChangeText={setNome}
+      />
+
+      <Text></Text>Email:</Text>
+      <TextInput
+        value={email}
+        onChangeText={setEmail}
+      />
+
+      <Text>Senha:</Text>
+      <TextInput
+        value={senha}
+        onChangeText={setSenha}
+        secureTextEntry
+      />
+
+      <Text>Cpf</Text>
+      <TextInput
+        value={Cpf}
+        onChangeText={setCpf}
+      />
+
+      <Button
+        title="Cadastrar"
+        onPress={handleCadastro}
+      />
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default Cadastro;
